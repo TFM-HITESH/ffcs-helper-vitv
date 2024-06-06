be2a8f2f4c208f4c548b9c3220cc9b78c4c8c781
TFM-HITESH <hiteshshivkumar@gmail.com>
2024-06-07 02:47:47 +0530

    Quickfix : Color of hover Inputs changed

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -27,7 +27,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           background: useMotionTemplate`
         radial-gradient(
           ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
-          var(--blue-500),
+          var(--neutral-400),
           transparent 80%
         )
       `,
