0734b7eff640c59e2c253d5ac18cfc34c36a76e8
hrsvrn <harshvardhanvatsa@gmail.com>
2024-06-07 11:18:17 +0530

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+'use client'
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+
+class Footer extends Component {
+  render() {
+    const current_year = new Date().getFullYear();
+    return (
+      <div id="section_footer">
+        <div className='text-center py-4'>
+          <p>Copyright © {current_year},. All Rights Reserved.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
