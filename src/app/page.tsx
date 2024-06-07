3b22d217d90a184f1091184f2d69d08794ca81dd
hrsvrn <harshvardhanvatsa@gmail.com>
2024-06-08 01:20:27 +0530

    Added Front Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
-    <main className="text-4xl flex flex-col ">
-      Hello World
-      <Link href="/dashboard" className="bg-red-500 w-[50%]">
-        /dashboard
-      </Link>
-      <Link href="/authSignup" className="bg-blue-500 w-[50%]">
-        /authSignup
-      </Link>
-      <Link href="/authLogin" className="bg-green-500 w-[50%]">
-        /authLogin
-      </Link>
-    </main>
+    <section className="body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-col text-center w-full mb-20">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 ">FFCS Helper</h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+    </div>
+    <div className="flex flex-wrap">
+      
+      
+    </div>
+    <Button className="flex mx-auto mt-16 border-0 py-2 px-8" variant="default"><a href="/authSignup">Sign Up</a></Button>
+  </div>
+</section>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ class Footer extends Component {
   render() {
     const current_year = new Date().getFullYear();
     return (
-      <div className="fixed bottom-0    ">
+      <div className="fixed bottom-0 ">
           <p>Copyright © {current_year},. All Rights Reserved.</p>
         </div>
     );
