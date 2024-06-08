02dd15de04fb71264d9faf2030978ee7cb0798b5
hrsvrn <harshvardhanvatsa@gmail.com>
2024-06-08 18:57:42 +0530

    changed the text body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
     <section className="body-font">
   <div className="container px-5 py-24 mx-auto bg-dark rounded-md">
     <div className="flex flex-col text-center w-full mb-20">
-      <h1 className="text-2xl font-medium title-font mb-4 text-lighter">Welcome to FFCS Time Table Manager</h1>
-      <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-lightest">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <h1 className="text-2xl font-medium title-font mb-4 text-gray-900">Welcome to FFCS Time Table Manager</h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-lightest">Create your dream timetable with a few simple clicks!</p>
     </div>
     <div className="flex flex-wrap">
       
